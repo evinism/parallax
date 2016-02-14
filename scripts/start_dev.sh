e02db3128e6f1478b63b93f4a3b3e0d98a41120d
Evin Sellin <evinism@gmail.com>
2016-02-14 10:46:40 -0800

    add redis dep

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# you can just... add things!
 npm run watch &
+npm run redis-dev &
 npm run server:development
+
 echo "Cleaning up..."
 kill %1 #Clean up on server die
+kill %2
 wait
 sleep 1
